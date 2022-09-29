4e8c4509c099bdc84ba289b2b21a7507a7f967ba
NastyaKatyushkina <nastyusha_kat@mail.ru>
2022-09-29 11:52:36 +0500

    homework 1.2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,9 +1,26 @@
-const addText = function () {
-    const output = document.getElementById("timer");
-    output.textContent -= 1;
-    if (output.textContent <= 0) {
-        alert('Вы победили в конкурсе');
-        clearInterval(inervalID);
-    }
+const deadCount = document.getElementById('dead');
+const lostCount = document.getElementById('lost');
+
+function hole(index) {
+    return document.getElementById(`hole${index}`);
+  }
+  
+for (let i = 1; i <= 9; i++) {
+    hole(i).onclick = () => {
+        if (hole(i).classList.contains('hole_has-mole')) {
+            deadCount.textContent++;
+        } else {
+            lostCount.textContent++;
+        }
+
+        if (deadCount.textContent === '10') {
+            alert('Вы выиграли!');
+            deadCount.textContent = 0;
+            lostCount.textContent = 0;
+        } else if (lostCount.textContent === '5') {
+            alert('Вы проиграли!');
+            deadCount.textContent = 0;
+            lostCount.textContent = 0;
+        }
+    }    
 }
-const inervalID = setInterval(addText, 1000);
