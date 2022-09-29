bb74d351d6eb6d3e0ce9bfc3ef3380ba2896889c
NastyaKatyushkina <nastyusha_kat@mail.ru>
2022-09-29 12:09:38 +0500

    homework_2

---
+++ element-search/menu/task.js
@@ -0,0 +1,17 @@
+const menuItem = Array.from(document.querySelectorAll(".menu__link"));
+const menu = Array.from(document.querySelectorAll(".menu"));
+
+menuItem.forEach((element) => {
+	element.onclick = function () {
+		if (element.parentElement.querySelector(".menu")) {
+			element.parentElement.querySelector(".menu").classList.toggle("menu_active");
+			return false;
+		};
+
+		menu.forEach((element) => {
+			if (element.classList.contains("menu_active")) {
+				element.classList.remove("menu_active");
+			}
+		});
+	};
+});
---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+const modalMain = document.getElementById('modal_main');
+const closed = document.querySelectorAll('.modal__close');
+modalMain.classList.add('modal_active');
+for (let i of closed) {
+    i.onclick = function () {
+        modal = this.closest('.modal');
+        modal.classList.remove('modal_active');
+    }
+}
+const modalSuccess = document.getElementById('modal_success');
+const showSuccess = modalMain.querySelector('.show-success');
+
+showSuccess.onclick = function () {
+    modal = this.closest('.modal');
+    modal.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
+}
